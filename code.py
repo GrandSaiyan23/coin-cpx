b84fa56d80b2912ca0474d9698bbf626dc83c505
GrandSaiyan23 <42147529+GrandSaiyan23@users.noreply.github.com>
2018-08-09 11:43:11 -0500

    deleted unnecessary code

--- code.py
+++ code.py
@@ -1,13 +1,9 @@
 from adafruit_circuitplayground.express import cpx
 import time
 import simpleio
-pink = (40, 0, 30)
-turquoise = (34, 125, 108)
-yellow = (40, 20, 0)
-off = (0, 0, 0) 
+yellow = (40, 20, 0) 
 while True:
     if cpx.button_a:
         cpx.pixels.fill(yellow)
     if cpx.button_b:
-         cpx.play_file("coin.wav")
-    
+         cpx.play_file("coin.wav"
